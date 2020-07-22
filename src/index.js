cc8cfe14e6aa33a4e623ed94e37427a2ec3bfbde
Demian <waltondemian@gmail.com>
2020-07-22 13:02:50 -0300

    Se arregla modificacion productos.

--- src/index.js
+++ src/index.js
@@ -8,26 +8,28 @@ const methodOverride = require('method-override');
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
 //Motor de plantillas
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 //URL encode  - Para que nos pueda llegar la información desde el formulario al req.body
-app.use(express.urlencoded({ extended: false }));
-
-//URL encode  - Agarra la informacion del formulario con req.body
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 
 //Capturar datos que vienen de formularios
 app.use(express.urlencoded({
     extended: false
 }));
 //app.use(express.json());
+//Middleware para manejar los metodos PUT/DELETE
+app.use(methodOverride('_method'));
 
 //Traigo el archivo de rutas y lo implemento
 const webRoutes = require('./router/web');
 const productRoutes = require('./router/products');
+const adminRoutes = require('./router/admin');
+
 app.use(webRoutes);
 app.use(productRoutes);
-const adminRoutes = require('./router/admin');
 app.use(adminRoutes);
-app.use(methodOverride('_method'));
+
 
 app.listen(2000, 'localhost', () => console.log('Servidor corriendo en el puerto 2000'));
