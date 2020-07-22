99c7526396a1bd4b7fc2c598a579e35718f33dfc
Fran <franco.colavitta@hotmail.com>
2020-07-22 11:11:07 -0300

    Crude administrador
    Crud, adminitrador, edit, delete

---
+++ src/controllers/controllersAdmin.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const fs = require('fs');
+let notebooks =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','notebooks.json')));
+
+//Render con ejs, sendFile con html.
+module.exports = {
+    admin: function (req, res) {
+        res.render(path.resolve(__dirname, '..', 'views', 'admin', 'administrar'),{notebooks});
+    },
+    create: (req, res) =>{
+        let relojes =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','notebooks.json')));
+        res.render(path.resolve(__dirname, '..','views','admin','create'));
+    },
+    save: (req,res)=>{
+        let relojes =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','notebooks.json')));
+        //res.send(req.body);
+        //console.log(req.body);
+        //return res.send(req.file);
+        let nuevoProducto={
+            id: notebooks.length + 1,
+            nombre: req.body.nombre,
+            marca: req.body.marca,
+            modelo: req.body.modelo,
+            descripcion: req.body.descripcion,
+            precio: req.body.precio,
+
+        };
+            //Aquí se agrega al array el nuevo Producto
+            notebooks.push(nuevoProducto);
+            //Convertir mi array en un string
+            let nuevoProductoGuardar = JSON.stringify(notebooks,null,2)
+            //Guardar o reemplazar nuestro archivo JSON
+            fs.writeFileSync(path.resolve(__dirname,'..','data','notebooks.json'), nuevoProductoGuardar);
+            res.redirect('/administrar');
+    },
+    show: (req,res)=>{
+        let relojes =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','notebooks.json')));
+        //res.send(req.params.id);
+        let miNotebook;
+        notebooks.forEach(notebook => {
+           if(notebook.id == req.params.id){
+               miNotebook = notebook;         
+            }
+        });
+        res.render(path.resolve(__dirname, '..','views','productos','productDetails'), {miNotebook})
+    
+    },
+    destroy: (req,res) =>{
+        let relojes =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','notebooks.json')));
+        const notebookDeleteId = req.params.id; 
+        const notebookFinal = notebooks.filter(notebook => notebook.id != notebookDeleteId);
+        let notebooksGuardar = JSON.stringify(notebookFinal,null,2);
+        //Guardar o reemplazar nuestro archivo JSON
+        fs.writeFileSync(path.resolve(__dirname,'..','data','notebooks.json'), notebooksGuardar);
+        res.redirect('/administrar');
+    },   
+    edit: (req,res) =>{
+        let notebooks =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','notebooks.json')));
+        const notebookId = req.params.id;
+        let notebookEditar = notebooks.find(notebook => notebook.id == notebookId);
+        res.render(path.resolve(__dirname, '..','views','admin','edit'), {notebookEditar});
+    },
+    update: (req,res) =>{
+        let notebooks =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','notebooks.json')));
+        req.body.id = req.params.id;
+        req.body.imagen = req.file ? req.file.filename : req.body.oldImagen;
+        let notebooksUpdate = notebooks.map(notebook => {
+            if(notebook.id  == req.body.id){
+                return notebook = req.body;
+            }
+            return notebook;
+        });
+        let notebooksActualizar = JSON.stringify(notebooksUpdate,null,2);
+        //Guardar o reemplazar nuestro archivo JSON
+        fs.writeFileSync(path.resolve(__dirname,'..','data','notebooks.json'), notebooksActualizar);
+        res.redirect('/administrar');
+
+    }
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
 
 
 //Archivos estaticos para express.
@@ -8,12 +9,16 @@ app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
 //Motor de plantillas
 app.set('view engine','ejs');
+//URL encode  - Para que nos pueda llegar la información desde el formulario al req.body
+app.use(express.urlencoded({ extended: false }));
 
 //Traigo el archivo de rutas y lo implemento
 const webRoutes = require('./router/web');
 app.use(webRoutes);
 const productRoutes = require('./router/products');
 app.use(productRoutes);
-
+const adminRoutes = require('./router/admin');
+app.use(adminRoutes);
+app.use(methodOverride('_method'));
 
 app.listen(2000, 'localhost', () => console.log('Servidor corriendo en el puerto 3000'));
